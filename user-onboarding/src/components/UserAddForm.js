0eca22ae4a74011f505d41f5bb0a16dd4722597c
Kiran Junnarkar <kjunnarkar@yahoo.com>
2019-12-12 15:13:28 -0800

    Error when rendering page though code compiles successfully

---
+++ user-onboarding/src/components/UserAddForm.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Form, Field } from 'formik';
+//import * as Yup from 'yup';
+//import axios from 'axios';
+//import styled from 'styled-components';
+
+// Styled Components
+
+
+
+const UserAddForm = props => {
+    return (
+        <div>
+            {/* Create Forms */}
+          {/*  <Form>
+                <Field type='text' name='name' placeholder='Enter Name' />
+                <Field type='email' name='email' placeholder='Enter Email' />
+                <Field type='password' name='password' placeholder='Password' />
+                <label>
+                    'Accept Terms of Service'
+                    <Field 
+                        type='checkbox' 
+                        name='terms' 
+                    />
+                </label>
+                <button type='submit'>Submit</button>
+          </Form> */}
+        </div>
+    );
+};
+export default UserAddForm;
